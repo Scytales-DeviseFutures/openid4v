ea93ff4a39e3c3cc903e43809881a6d1d2075930
Luis Pereira <luismpereira23@gmail.com>
2024-06-26 14:27:38 +0100

    update 2 - add verify token to credential

--- src/openid4v/openid_credential_issuer/credential.py
+++ src/openid4v/openid_credential_issuer/credential.py
@@ -1,8 +1,10 @@
 import logging
 from typing import Optional
 from typing import Union
+from datetime import datetime
 
 from cryptojwt.jws.jws import factory
+from cryptojwt.jwt import utc_time_sans_frac
 import requests
 from fedservice.entity.utils import get_federation_entity
 from idpyoidc.exception import RequestError
@@ -266,6 +268,47 @@ class Credential(Endpoint):
         }
 
         return {"response_args": _resp, "client_id": _session_info["client_id"]} """
+
+    def verify_token_and_authentication(self, request):
+        _mngr = self.upstream_get("context").session_manager
+        try:
+            _session_info = _mngr.get_session_info_by_token(
+                request["access_token"], grant=True, handler_key="access_token"
+            )
+        except (KeyError, ValueError):
+            return self.error_cls(
+                error="invalid_token", error_description="Invalid Token"
+            )
+
+        _grant = _session_info["grant"]
+        token = _grant.get_token(request["access_token"])
+        # should be an access token
+        if token and token.token_class != "access_token":
+            return self.error_cls(
+                error="invalid_token", error_description="Wrong type of token"
+            )
+
+        # And it should be valid
+        if token.is_active() is False:
+            return self.error_cls(
+                error="invalid_token", error_description="Invalid Token"
+            )
+
+        _auth_event = _grant.authentication_event
+        # if the authentication is still active or offline_access is granted.
+        if not _auth_event["valid_until"] >= utc_time_sans_frac():
+            logger.debug(
+                "authentication not valid: {} > {}".format(
+                    datetime.fromtimestamp(_auth_event["valid_until"]),
+                    datetime.fromtimestamp(utc_time_sans_frac()),
+                )
+            )
+            return False, None
+
+            # This has to be made more finegrained.
+            # if "offline_access" in session["authn_req"]["scope"]:
+            #     pass
+        return True, _session_info["client_id"]
 
     # gets the public key from a JWK
     def pKfromJWK(self, jwt_encoded):
